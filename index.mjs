c2dd6dd85922415561899d4f92d797355de74ff6
gabrielstuff <gabriel@soixantecircuits.fr>
2021-09-14 09:43:04 +0200

    💎 add http Koa mini server to allow remote debug

--- index.mjs
+++ index.mjs
@@ -2,6 +2,9 @@ import internalIp from 'internal-ip'
 import osc from 'osc'
 import { map } from 'map-number'
 import isPi from 'detect-rpi'
+import Koa from 'koa'
+import Router from '@koa/router'
+
 let MotorHat = null
 let motorHat = null
 ;(async () => {
@@ -11,8 +14,54 @@ let motorHat = null
   }
 })();
 
-const port = 57121
+let OSCOpen = false
+let OSCTimeLastReceive = Date.now()
+let OSCLastError = ''
+let localIp = ''
+let motorSpeed = ''
+const httpPort = 8080
+const OSCport = 57121
 const mainWave = 'delta'
+const app = new Koa()
+const router = new Router()
+
+router.get('/status/osc', (ctx, next) => {
+  ctx.body = {
+    status: OSCOpen,
+    receiving: OSCReceiving(),
+    lastReceive: OSCTimeLastReceive,
+    lastError: OSCLastError
+  }
+})
+
+router.get('/status/motor', (ctx, next) => {
+  ctx.body = {
+    motorSpeed,
+    motorHat
+  }
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.use(async (ctx, next) => {
+  if(parseInt(ctx.status) === 404){
+    ctx.status = 404
+    ctx.body = {
+      msg:'emmmmmmm, seems 404',
+      routes: {
+        osc: `http://${localIp}:${httpPort}/status/osc`,
+        motor: `http://${localIp}:${httpPort}/status/motor`
+      },
+      conf: {
+        localIp,
+        OSCport,
+        mainWave
+      }
+    }
+  }
+})
 
 const average = values => {
   if (values.length) {
@@ -34,23 +83,28 @@ const waveValueThreshold = (listOfData, min, max) => {
   return listOfData.filter(el => (el > min) && (el < max))
 }
 
+const OSCReceiving = () => {
+  return (Date.now() - OSCTimeLastReceive) < 150 
+}
+
 ;(async () => {
-  const localIp = await internalIp.v4()
+  localIp = await internalIp.v4()
   var udpPort = new osc.UDPPort({
     localAddress: localIp,
-    localPort: port,
+    localPort: OSCport,
     metadata: true
   })
   if (motorHat) {
     console.log('Start Motor control over I2C')
     motorHat.init()
   } else {
-    console.log('no motor available')
+    console.warn('!! no motor available !!')
   }
   // Listen for incoming OSC messages.
   udpPort.on('message', function (oscMsg, timeTag, info) {
     // console.log('An OSC message just arrived!', oscMsg)
     // console.log('Remote info is: ', info)
+    OSCTimeLastReceive = Date.now()
     if (oscMsg.address && oscMsg.address.indexOf('absolute') > 1) {
       const waveName = oscMsg.address.split('/').pop().replace('_absolute', '')
       const waveValue = oscMsg.args
@@ -70,9 +124,13 @@ const waveValueThreshold = (listOfData, min, max) => {
         const mappedValue = map(waveValueAverage, -1, 1, 0, 1)
         console.log(`MAPPED - ${waveName}: `, mappedValue)
         console.log(`---`)
-        const motorSpeed = mappedValue * 10
+        // HERE WE SET THE MOTOR SPEED
+        // WE CEIL THE VALUE TO 2 number behind the dot : 0.6581954509019852 become 0.66
+        //
+        motorSpeed = Number((mappedValue * 10).toFixed(2))
         let direction = 'back'
         if (motorHat) {
+          console.log('controlling motor')
           motorHat.dcs[0].setSpeedSync(motorSpeed)
           if (mappedValue < 0.50) {
             direction = 'back'
@@ -96,8 +154,31 @@ const waveValueThreshold = (listOfData, min, max) => {
     }
   })
   udpPort.on('ready', function () {
-    console.log(`OSC listening on ${localIp}:${port}`)
+    console.log(`OSC listening on ${localIp}:${OSCport}`)
+    OSCOpen = true
+  })
+  udpPort.on('error', function (error) {
+    console.log("An error occurred: ", error.message)
+    OSCLastError = error.message
   })
   // Open the socket.
   udpPort.open()
+
+  // response
+  app.use(ctx => {
+    ctx.body = {
+      msg:'Hello Reve quantique',
+      routes: {
+        osc: `http://${localIp}:${httpPort}/status/osc`,
+      },
+      conf: {
+        localIp,
+        OSCport,
+        mainWave
+      }
+    }
+  })
+
+  app.listen(httpPort)
+  console.log(`http status running on : http://${localIp}:${httpPort}`)
 })()
